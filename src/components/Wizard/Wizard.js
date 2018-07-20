f01112d1ab02a5c4225dd9da0bad0a053b6631a5
Josh <joshbrogdon5@gmail.com>
2018-07-20 11:07:55 -0600

    step 2 done

--- src/components/Wizard/Wizard.js
+++ src/components/Wizard/Wizard.js
@@ -12,17 +12,48 @@ export default class Wizard extends Component {
             zipcode: ''
         }
     }
+
+    handleName(val){
+        this.setState({
+            name: val
+        })
+    }
+
+    handleAddress(val){
+        this.setState({
+            address: val
+        })
+    }
+
+    handleCity(val){
+        this.setState({
+            city: val
+        })
+    }
+
+    handleState(val){
+        this.setState({
+            state: val
+        })
+    }
+
+    handleZipcode(val){
+        this.setState({
+            zipcode: val
+        })
+    }
+
     render(){
         return(
             <div>
                 Wizard Component
                 <Link to="/"><button>Cancel</button></Link>
                 <div className="inputs">
-                    <input placeholder="name" />
-                    <input placeholder="address" />
-                    <input placeholder="city" />
-                    <input placeholder="state" />
-                    <input placeholder="zipcode" />
+                    <input placeholder="name" value={this.state.name} onChange={(e) => this.handleName(e.target.value)} type="text"/>
+                    <input placeholder="address" value={this.state.address} onChange={(e) => this.handleAddress(e.target.value)} type="text"/>
+                    <input placeholder="city" value={this.state.city} onChange={(e) => this.handleCity(e.target.value)} type="text"/>
+                    <input placeholder="state" value={this.state.state} onChange={(e) => this.handleState(e.target.value)} type="text"/>
+                    <input placeholder="zipcode" value={this.state.zipcode} onChange={(e) => this.handleZipcode(e.target.value)} type="text"/>
                 </div>
             </div>
         )
