cbd9420202ba97c938d7d0cdd5e01cd9b3ea09e2
Josh <joshbrogdon5@gmail.com>
2018-07-20 16:06:55 -0600

    Reducer not finished but working

--- src/components/Wizard/Wizard.js
+++ src/components/Wizard/Wizard.js
@@ -1,76 +1,57 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 import axios from 'axios';
+import { connect } from 'react-redux';
+import {handleName, handleAddress, handleCity, handleState, handleZipcode} from '../../ducks/reducer';
 
-export default class Wizard extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            name: '',
-            address: '',
-            city: '',
-            state: '',
-            zipcode: ''
-        }
-        this.complete=this.complete.bind(this);
-    }
+class Wizard extends Component {
 
-    complete(){
-        axios.post('http://localhost:3333/api/house').then(results => {
-            this.setState({
-                name: this.handleName,
-                address: this.handleAddress,
-                city: this.handleCity,
-                state: this.handleState,
-                zipcode: this. handleZipcode
-            })
-        })
-    }
 
-    handleName(val){
-        this.setState({
-            name: val
-        })
-    }
+    
 
-    handleAddress(val){
-        this.setState({
-            address: val
-        })
-    }
+    // complete(){
+    //     axios.post('http://localhost:3333/api/house').then(results => {
+    //         this.setState({
+    //             name: this.handleName,
+    //             address: this.handleAddress,
+    //             city: this.handleCity,
+    //             state: this.handleState,
+    //             zipcode: this. handleZipcode
+    //         })
+    //     })
+    // }
 
-    handleCity(val){
-        this.setState({
-            city: val
-        })
-    }
-
-    handleState(val){
-        this.setState({
-            state: val
-        })
-    }
-
-    handleZipcode(val){
-        this.setState({
-            zipcode: val
-        })
-    }
+   
 
     render(){
+        const {handleName, handleAddress, handleCity, handleState, handleZipcode} = this.props;
         return(
             <div>
                 Wizard Component
                 <Link to="/"><button>Cancel</button></Link>
                 <div className="inputs">
-                    <input placeholder="name" value={this.state.name} onChange={(e) => this.handleName(e.target.value)} type="text"/>
-                    <input placeholder="address" value={this.state.address} onChange={(e) => this.handleAddress(e.target.value)} type="text"/>
-                    <input placeholder="city" value={this.state.city} onChange={(e) => this.handleCity(e.target.value)} type="text"/>
-                    <input placeholder="state" value={this.state.state} onChange={(e) => this.handleState(e.target.value)} type="text"/>
-                    <input placeholder="zipcode" value={this.state.zipcode} onChange={(e) => this.handleZipcode(e.target.value)} type="text"/>
+                    <input placeholder="name"  onChange={(e) => handleName(e.target.value)} type="text"/>
+                    <input placeholder="address"  onChange={(e) => handleAddress(e.target.value)} type="text"/>
+                    <input placeholder="city"  onChange={(e) => handleCity(e.target.value)} type="text"/>
+                    <input placeholder="state"  onChange={(e) => handleState(e.target.value)} type="text"/>
+                    <input placeholder="zipcode"  onChange={(e) => handleZipcode(e.target.value)} type="text"/>
                     <button onClick={this.complete}>Complete</button>
                 </div>
             </div>
         )
     }
 }
+
+function mapStatetoProps(state){
+    const {handleName, handleAddress, handleCity, handleState, handleZipcode} = state;
+    return{
+        handleName, 
+        handleAddress,
+        handleCity, 
+        handleState, 
+        handleZipcode
+    }
+}
+
+
+export default connect(mapStatetoProps, {handleName, handleAddress, handleCity, handleState, handleZipcode})(Wizard);
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -7,7 +7,10 @@ const initialState = {
 }
 
 const UPDATE_NAME = 'UPDATE_NAME';
-const UPDATE_ADDRESS = 'UPDATE_ADDRESS'
+const UPDATE_ADDRESS = 'UPDATE_ADDRESS';
+const UPDATE_CITY = 'UPDATE_CITY';
+const UPDATE_STATE = 'UPDATE_STATE';
+const UPDATE_ZIPCODE = 'UPDATE_ZIPCODE'
 
 export function handleName(name){
     return {
@@ -16,10 +19,50 @@ export function handleName(name){
     }
 }
 
+export function handleAddress(address){
+    return {
+        type: UPDATE_ADDRESS,
+        payload: address
+    }
+}
+
+export function handleCity(city){
+    return {
+        type: UPDATE_CITY,
+        payload: city
+    }
+}
+
+export function handleState(state){
+    return {
+        type: UPDATE_STATE,
+        payload: state
+    }
+}
+
+export function handleZipcode(zipcode){
+    return {
+        type: UPDATE_ZIPCODE,
+        payload: zipcode
+    }
+}
+
 function reducer(state = initialState, action){
     switch(action.type){
         case UPDATE_NAME:
             return Object.assign({}, state, {name: action.payload})
+
+        case UPDATE_ADDRESS:
+            return Object.assign({}, state, {address: action.payload})
+
+        case UPDATE_CITY:
+            return Object.assign({}, state, {city: action.payload})
+
+        case UPDATE_STATE:
+            return Object.assign({}, state, {state: action.payload})
+
+        case UPDATE_ZIPCODE:
+            return Object.assign({}, state, {zipcode: action.payload})
 
         default: return state;
     }
